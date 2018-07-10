e387a1abbb822e284dadc3c66ffefd77b0dff749
Adriano Dantas <adrianodts@msn.com>
2018-07-10 00:40:37 -0300

    FlowScoped

---
+++ maratona-jsf/src/br/com/devdojo/maratonajsf/adriano/bean/flow/TesteFlowNestedBean.java
@@ -0,0 +1,45 @@
+package br.com.devdojo.maratonajsf.adriano.bean.flow;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.flow.FlowScoped;
+import javax.inject.Named;
+
+@Named
+@FlowScoped(value="pendencie")
+public class TesteFlowNestedBean implements Serializable {
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String userName;
+	private String userSurname;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserSurname() {
+		return userSurname;
+	}
+	public void setUserSurname(String userSurname) {
+		this.userSurname = userSurname;
+	}
+	
+	public String validarUser() {
+		System.out.println("Fazendo consulta na SERASA");
+		System.out.println("Fazendo consulta no SPC");
+		if(true) {
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuário não passou nas pendências", "Algum detalge útil"));
+			return null;
+		}
+		return "proceedToRegistration";
+	}
+}
